666478f0eccdc9cf5d6fc3204138b868e4dac664
Ricardo Casares <ricardocasares@gmail.com>
2018-01-23 16:00:53 +0100

    Code readability

--- functions/arguman/intents/expand.js
+++ functions/arguman/intents/expand.js
@@ -1,30 +1,29 @@
 const log = require("debug")("app:intent:expand");
 const { premise, hasType } = require("../lib");
 
-module.exports = function(app) {
-  app.intent(
-    "ExpandIntent",
-    {
-      utterances: [
-        "Why",
-        "How",
-        "Expand",
-        "Tell me more about it",
-        "Please expand",
-        "But what",
-        "Because"
-      ]
-    },
-    (req, res) => {
-      const store = req.getSession();
-      const contention = store.get("contention");
-      const { absolute_url: url } = contention;
-
-      log(contention);
-
-      return premise(`${url}.json`).then(({ type, premise }) => {
-        return res.say(hasType(premise, type)).shouldEndSession(false);
-      });
-    }
-  );
+const NAME = "ExpandIntent";
+const MODEL = {
+  utterances: [
+    "Why",
+    "How",
+    "Expand",
+    "Tell me more about it",
+    "Please expand",
+    "But what",
+    "Because"
+  ]
 };
+
+function handler(req, res) {
+  const store = req.getSession();
+  const contention = store.get("contention");
+  const { absolute_url: url } = contention;
+
+  log(contention);
+
+  return premise(`${url}.json`).then(({ type, premise }) => {
+    return res.say(hasType(premise, type)).shouldEndSession(false);
+  });
+}
+
+module.exports = app => app.intent(NAME, MODEL, handler);
--- functions/arguman/intents/topic.js
+++ functions/arguman/intents/topic.js
@@ -1,33 +1,32 @@
 const log = require("debug")("app:intent:topic");
 const { pick } = require("../lib");
 
-module.exports = function(app) {
-  app.intent(
-    "TopicIntent",
-    {
-      utterances: [
-        "Pick an argument",
-        "Another one",
-        "Choose another one",
-        "Choose a topic",
-        "Find a discussion",
-        "Let's talk about something",
-        "Let's discuss",
-        "Tell me something",
-        "Select a topic",
-        "Amuse me",
-        "Say something interesting"
-      ]
-    },
-    (req, res) => {
-      const store = req.getSession();
-
-      return pick().then(contention => {
-        log(contention);
-        const { title } = contention;
-        store.set("contention", contention);
-        return res.say(title).shouldEndSession(false);
-      });
-    }
-  );
+const NAME = "TopicIntent";
+const MODEL = {
+  utterances: [
+    "Pick an argument",
+    "Another one",
+    "Choose another one",
+    "Choose a topic",
+    "Find a discussion",
+    "Let's talk about something",
+    "Let's discuss",
+    "Tell me something",
+    "Select a topic",
+    "Amuse me",
+    "Say something interesting"
+  ]
 };
+
+function handler(req, res) {
+  const store = req.getSession();
+
+  return pick().then(contention => {
+    log(contention);
+    const { title } = contention;
+    store.set("contention", contention);
+    return res.say(title).shouldEndSession(false);
+  });
+}
+
+module.exports = app => app.intent(NAME, MODEL, handler);
